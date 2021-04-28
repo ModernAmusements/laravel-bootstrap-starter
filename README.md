afb3122c308ed5069e8a8327db7bbb4814e7a9d5
ModernAmusements <st@zahnarzt-helden.de>
2021-04-28 19:03:31 +0200

    laravel stater-kit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Laravel Mix Starter Kit - Shady Tawfik
+
+## What's inside?
+
+It's a pretty simple project, it's a starter kit based on [Laravel Mix](https://github.com/JeffreyWay/laravel-mix)
+
+Include: 
+
+- Vue
+- Bootstrap 4
+- Axios
+- Popper.js
+- lodash
+- jQuery
+- cross-env
+
+## Installation
+
+1. `npm install`
+2. `npm run dev`
+3. Add `mix.browserSync()` to `webpack.mix.js`
+
+## License
+
+Laravel Mix is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+mix.js('assets/js/app.js', 'public/js');
+mix.sass('assets/sass/app.scss', 'public/css');
+
+mix.browserSync('https://laravel-mix-kit.test/')
+
+
+if (mix.inProduction()) {
+    mix.version()
+       .sourceMaps();
+  }
